4eed574e26f5ee1e7009da57ddb7562afdd31728
Sharp-rookie <1842604700@qq.com>
2022-03-26 11:36:00 +0800

    quick push

--- lab2 my_torch/half_moon_mytorch.py
+++ lab2 my_torch/half_moon_mytorch.py
@@ -1,3 +1,105 @@
 import ipdb
 import mytorch
 from mytorch import my_tensor
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+
+np.random.seed(729)
+lr = 0.01
+epoch_num = 100
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model_path = "torch_model.ckpt"
+
+
+class Net(mytorch.Module):
+    def __init__(self, input_size=2):
+        super(Net, self).__init__()
+
+        # layers
+        self.fc1 = mytorch.Linear(in_features=input_size, out_features=5)
+        self.fc2 = mytorch.Linear(in_features=5, out_features=5)
+        self.fc3 = mytorch.Linear(in_features=5, out_features=1)
+        self.relu = mytorch.Functional.ReLU()
+        self.sigmoid = mytorch.Functional.Sigmoid()
+
+        self.parameters = [self.fc1.w, self.fc2.w, self.fc3.w]
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.sigmoid(out)
+        out = self.fc3(out)
+        out = out.squeeze(-1)
+
+        return out
+
+
+def generate_data():
+    X, y = datasets.make_moons(n_samples=500, shuffle=True, noise=0.2)
+    return X, y
+
+
+def show_data(X, y):
+    plt.figure()
+    plt.scatter(X[:, 0], X[:, 1], c=y, marker=".")
+    plt.show()
+
+
+def mertix(predict, y):
+    index = (np.array(predict) > 0.5).reshape(-1)
+    if index.sum() != 0:
+        P = y[index].sum() / index.sum()
+        R = y[index].sum() / y.sum()
+    else:
+        P = 0
+        R = 0
+
+    return P, R
+
+
+def test(model, x_test, y_test):
+    """
+    Return the precise and recall on the test dataset
+    """
+
+
+def main():
+
+    X, y = generate_data()
+    x_train, x_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.1, shuffle=True)
+    # show_data(x_test, y_test)
+
+    model = Net(X.shape[1])
+    optimizer = mytorch.Optim.SGD(model.parameters, lr, momentum=0.9)
+    criterion = mytorch.Functional.MSELoss()
+
+    # Train
+    P_, R_ = 0, 0
+    for epoch in tqdm(range(epoch_num)):
+        # print("Epoch[%d]" % epoch)
+        P, R = test(model, x_test, y_test)
+
+        for _, (x, y) in enumerate(zip(x_train, y_train)):
+            x = my_tensor.tensor(x)
+            y = my_tensor.tensor(y)
+
+            # Forward Pass
+            output = model.forward(x)
+            loss = criterion(output, y)
+
+            # Backward and Optimize
+            loss.backward()
+            optimizer.step()
+
+    # Test
+    P, R = test(model, x_test, y_test)
+    print(" Precise: %.2f%%   Recall: %.2f%%" % (P*100, R*100))
+
+
+if __name__ == "__main__":
+    main()
