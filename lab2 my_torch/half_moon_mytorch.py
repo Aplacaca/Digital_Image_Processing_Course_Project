775483d249fcc1ce4c3940efafdb0aff40f04a20
dhy <3057931787@qq.com>
2022-03-28 20:01:41 +0800

    add grad

--- lab2 my_torch/half_moon_mytorch.py
+++ lab2 my_torch/half_moon_mytorch.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
+from mytorch.myglobal import graph
+
 
 np.random.seed(729)
 lr = 0.01
@@ -14,7 +16,7 @@ epoch_num = 300
 model_path = "mytorch_model.ckpt"
 
 
-class Net(mytorch.Module):
+class Net(mytorch.Model):
     def __init__(self, input_size=2):
         super(Net, self).__init__()
 
@@ -28,13 +30,13 @@ class Net(mytorch.Module):
         self.parameters = [self.fc1.w, self.fc2.w, self.fc3.w]
 
     def forward(self, x):
+        # import pdb;pdb.set_trace()
         out = self.fc1(x)
         out = self.relu(out)
         out = self.fc2(out)
         out = self.sigmoid(out)
         out = self.fc3(out)
         out = out.squeeze(-1)
-
         return out
 
 
@@ -97,7 +99,8 @@ def main():
             loss = criterion(output, y)
 
             # Backward and Optimize
-            loss.backward(model)
+            model.backward(loss.backward())
+            # loss.backward(model)
             optimizer.step(model.parameters)
 
         if epoch % 10 == 1:
--- lab2 my_torch/mytorch/__init__.py
+++ lab2 my_torch/mytorch/__init__.py
@@ -1,3 +1,4 @@
 from .Modules import *
 from . import Optim
 from . import Functional
+from .myglobal import all_forward_dict
