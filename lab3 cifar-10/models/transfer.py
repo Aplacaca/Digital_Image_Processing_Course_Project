26c2656870d5a4cd341fbd726787a8322ad0c22c
Lrk612 <liruikun@dianintel.cn>
2022-04-11 13:07:27 +0800

    quick push

--- lab3 cifar-10/models/transfer.py
+++ lab3 cifar-10/models/transfer.py
@@ -6,5 +6,25 @@ from torch import nn
 from torchvision import models
 
 
-resnet18_transfer = models.resnet18(pretrained=False, num_classes=10)
+# resnet18 from torchvision
+resnet18_transfer = models.resnet18(pretrained=True, num_classes=10)
 resnet18_transfer.fc = nn.Linear(resnet18_transfer.fc.in_features, 10)
+# frozen all layers
+for p in resnet18_transfer.parameters():
+    p.requires_grad = False
+# unfrozen the last layer and fc
+for layer in [resnet18_transfer.layer4.parameters(), resnet18_transfer.fc.parameters()]:
+    for p in layer:
+        p.requires_grad = True
+
+
+# resnet50 from torchvision
+resnet50_transfer = models.resnet50(pretrained=True, num_classes=10)
+resnet50_transfer.fc = nn.Linear(resnet50_transfer.fc.in_features, 10)
+# frozen all layers
+for p in resnet50_transfer.parameters():
+    p.requires_grad = False
+# unfrozen the last layer and fc
+for layer in [resnet50_transfer.layer4.parameters(), resnet50_transfer.fc.parameters()]:
+    for p in layer:
+        p.requires_grad = True
