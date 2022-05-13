85f9445985bd7f6a67b5bf54a24345aa5e0fdc0a
root <root@SK-20210630CGVA.localdomain>
2022-05-13 22:26:33 +0800

    quick push

---
+++ lab5 lstm-gan/dataset.py
@@ -0,0 +1,80 @@
+# coding:utf-8
+import torch
+import pandas as pd
+from PIL import Image
+from torchvision import transforms
+from torch.utils.data import Dataset
+
+
+class ParseCSV:
+    """解析csv数据集"""
+
+    def __init__(self, csv_path):
+        self.csv_path = csv_path
+
+    def __call__(self):
+        """
+
+        Returns:
+        ------- 
+        data_list: List[List[str]]
+            数据集的路径列表，每组数据包含41张图片的路径
+        """
+
+        data_list = []
+        csv_data = pd.read_csv(self.csv_path)
+        for line in csv_data.values:
+            data_list.append(line.tolist())
+
+        return data_list
+
+
+class Weather_Dataset(Dataset):
+
+    def __init__(self, img_dir, csv_path, img_size):
+        self.img_dir = img_dir
+        self.data_list = ParseCSV(csv_path)()  # List[List[str]]  # ! TODO
+        self.transform = transform_aug = transforms.Compose([
+            transforms.Resize((img_size, img_size)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[.5], std=[.5])
+        ])
+
+    def __getitem__(self, index):
+        """
+
+        Returns:
+        ------- 
+        img: Tensor
+            一段时序图片，序列形状为(41, 1, 224, 224)
+        """
+
+        img_path_prefix = self.img_dir + '/' + \
+            self.img_dir.split('/')[-1].lower() + '_'
+
+        img_paths = [img_path_prefix + path for path in self.data_list[index]]
+
+        try:
+            imgs = [Image.open(img_path) for img_path in img_paths]
+        except:
+            print(img_paths)
+            raise Exception('Error: cannot open image')
+
+        if self.transform is not None:
+            imgs = [self.transform(img) for img in imgs]
+
+        imgs = torch.stack(imgs, dim=0)
+
+        return imgs
+
+    def __len__(self):
+        return len(self.data_list)
+
+
+if __name__ == '__main__':
+    dataset = Weather_Dataset(img_dir='weather_data/train/Precip',
+                              csv_path='weather_data/dataset_train.csv',
+                              img_size=224)
+
+    for i in range(len(dataset)):
+        print(dataset[i][40])
